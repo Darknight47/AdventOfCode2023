6540fba100724e74f584cbc6848ca4e16bf8e7e7
DarkNight <farhadgunners@gmail.com>
2023-12-06 22:36:21 +0100

    Day One

--- DayOne/Day.py
+++ DayOne/Day.py
@@ -1,26 +1,11 @@
-import re
 ans = 0
-number_words = {
-    'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
-    'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
-}
-
 try:
     # Open the file
     with open('input.txt', 'r') as file:
         # Read each line
         arr = []
-        # Create a regex pattern to match string numbers or single digits
-             
-        for line in file:
-            # Print the line
-            tempLine = line.strip()
-            pattern = r'\b(?:' + '|'.join(number_words.keys()) + r')\b|\d'  
-            # Find all matches in the input string
-            matches = re.findall(pattern, tempLine)
-            # Replace string numbers with their corresponding digits
-            separated_numbers = [number_words.get(match, match) for match in matches]
-            print("2")
+        #-- SECOND PART --#
+        
 except FileNotFoundError:
     print("The file does not exist")
 except IOError:
